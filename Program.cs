af522573f262ed4bc47fb3dd4cbf19c82beb4676
Baran Unsal <barannunsal@gmail.com>
2022-06-14 12:14:12 +0300

    Algorithm Questions

--- Program.cs
+++ Program.cs
@@ -15,47 +15,49 @@
 //Console.WriteLine($"Girdiğiniz sayi {num1}");
 //Console.WriteLine("2. sayi: ");
 //int num2 = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine($"Karelerinin toplamı: {(num1 * num1) + (num2*num2)} ");
+//Console.WriteLine($"Karelerinin toplamı: {(num1 * num1) + (num2 * num2)} ");
 //#endregion
 
 //#region Küplerinin toplamı
-////#region Çözüm 1
-////double result = 0;
-////for (int i = 1; i <= 10; i++)
-////{
-////    result += Math.Pow(i, 3);
-////}
+//#region Çözüm 1
+//double result = 0;
+//for (int i = 1; i <= 10; i++)
+//{
+//    result += Math.Pow(i, 3);
+//}
 
-////Console.WriteLine(result);
-////#endregion
+//Console.WriteLine(result);
+//#endregion
 
-////#region Çözüm 2
-////double result = 0;
-////foreach (var item in result.ToString())
-////{
-////    for (int i = 1; i < 11; i++)
-////        result += Math.Pow(i, 3);
-////}
-////Console.WriteLine(result);
-////#endregion
-//#region
-////double result = 0;
-////int i = 1;
-////while (i <= 10)
-////{
-////    result = Math.Pow(i, 3);
-////    i++;
-////}
-////Console.WriteLine(result);   
+//#region Çözüm 2
+//double result = 0;
+//foreach (var item in result.ToString())
+//{
+//    for (int i = 1; i < 11; i++)
+//        result += Math.Pow(i, 3);
+//}
+//Console.WriteLine(result);
+//#endregion
+
+//#region Cözüm 3
+//double result = 0;
+//int i = 1;
+//while (i <= 10)
+//{
+//    result = Math.Pow(i, 3);
+//    i++;
+//}
+//Console.WriteLine(result);
 
 //#endregion
+
 //#endregion
 
 //#region Doğum tarihi girilen kişinin yaş hesabı
-////Console.WriteLine("Lütfen doğum tarihini giriniz");
-////DateTime dateOfBirth = Convert.ToDateTime(Console.ReadLine());
-////TimeSpan result = DateTime.Now - dateOfBirth;
-////Console.WriteLine($"Yaşınız: {result.Days / 365}");
+//Console.WriteLine("Lütfen doğum tarihini giriniz");
+//DateTime dateOfBirth = Convert.ToDateTime(Console.ReadLine());
+//TimeSpan result = DateTime.Now - dateOfBirth;
+//Console.WriteLine($"Yaşınız: {result.Days / 365}");
 
 //Console.WriteLine("Lütfen doğum tarihini giriniz");
 //DateTime dogumTarihi = DateTime.Parse(Console.ReadLine());
@@ -75,22 +77,22 @@
 //    int factorial = 1;
 //    Console.WriteLine("Lütfen bir sayı giriniz:");
 //    int num = Convert.ToInt32(Console.ReadLine());
-//    //for (int i = num; i > 0; i--)
-//    //{
-//    //    factorial *= i;
-//    //}
+//    for (int i = num; i > 0; i--)
+//    {
+//        factorial *= i;
+//    }
 
-//    //while(num > 0)
-//    //{
-//    //    factorial *= num;
-//    //    num--;
-//    //}
+//    while (num > 0)
+//    {
+//        factorial *= num;
+//        num--;
+//    }
 
-//    //do
-//    //{
-//    //    factorial *= num;
-//    //    num--;
-//    //} while (num > 0);
+//    do
+//    {
+//        factorial *= num;
+//        num--;
+//    } while (num > 0);
 //    string result_ = "";
 //    while (num > 0)
 //    {
@@ -111,8 +113,8 @@
 //#endregion
 
 //#region Çarpma işlemi sonucunu toplama yaparak bulma
-//int num1;
-//int num2;
+//#region Çözüm 1
+
 //Console.WriteLine("birinci sayı");
 //num1 = Convert.ToInt32(Console.ReadLine());
 //Console.WriteLine($"İlk ({num1}) girdiğiniz sayı");
@@ -120,30 +122,37 @@
 //num2 = Convert.ToInt32(Console.ReadLine());
 //Console.WriteLine($"İkinci ({num2}) girdiğiniz sayı");
 
-////int carpma = 0;
-////for(int i = 0; i < num1; i++)
-////{
-////    carpma += num2;
-////}
-////Console.WriteLine(($"{num1} X {num2} = {carpma}"));
+//int carpma = 0;
+//for (int i = 0; i < num1; i++)
+//{
+//    carpma += num2;
+//}
+//Console.WriteLine(($"{num1} X {num2} = {carpma}"));
+//#endregion
 
-////int sayac = num2;
-////int sonuc = 0;
-////while (sayac > 0)
-////{
-////    sonuc += num1;
-////    sayac--;
-////}
-////Console.WriteLine($"{num1} X {num2} = {sonuc}");
+//#region Çözüm 2
+//int num1;
+//int num2;
+//int sayac = num2;
+//int sonuc = 0;
+//while (sayac > 0)
+//{
+//    sonuc += num1;
+//    sayac--;
+//}
+//Console.WriteLine($"{num1} X {num2} = {sonuc}");
+//#endregion
 
-////int result = 0;
-////int sayac = num2;
-////do
-////{
-////    result += num1;
-////    sayac--;
-////} while (sayac > 0);
-////Console.WriteLine(result);
+//#region Çözüm 2
+//int result = 0;
+//int sayac = num2;
+//do
+//{
+//    result += num1;
+//    sayac--;
+//} while (sayac > 0);
+//Console.WriteLine(result);
+//#endregion
 
 //#endregion
 
@@ -153,20 +162,20 @@
 //Console.WriteLine("İkinci sayi");
 //int num2 = Convert.ToInt32(Console.ReadLine());
 
-////int result = 0;
-////int num1_ = num1;
-////int kalan = 0;
-////for(int i = 0; i < num1_; i++)
-////{
-////    num1 -= num2;
-////    result++;
-////    if (num1 < num2)
-////    {
-////        kalan = num1;
-////        break;
-////    }
-////}
-////Console.WriteLine($"{num1_} / {num2} = {result} | Kalan {kalan}");
+//int result = 0;
+//int num1_ = num1;
+//int kalan = 0;
+//for (int i = 0; i < num1_; i++)
+//{
+//    num1 -= num2;
+//    result++;
+//    if (num1 < num2)
+//    {
+//        kalan = num1;
+//        break;
+//    }
+//}
+//Console.WriteLine($"{num1_} / {num2} = {result} | Kalan {kalan}");
 
 //int sayac = num1;
 //int result = 0;
@@ -184,16 +193,15 @@
 //#region girilen sayının kaç basamaklı olduğunu bulmak
 //Console.WriteLine("Lütfen 1 sayı giriniz: ");
 //int num = Convert.ToInt32(Console.ReadLine());
-////float num = float.Parse(Console.ReadLine());
-////int sayac = 0;
-////for (; num >= 10;)
-////{
-////    num /= 10;
-////    sayac++;
-////}
-////sayac++;
-////Console.WriteLine($"{num} değerinin basamak sayısı {sayac}");
-//int result = 0;    
+//int sayac = 0;
+//for (; num >= 10;)
+//{
+//    num /= 10;
+//    sayac++;
+//}
+//sayac++;
+//Console.WriteLine($"{num} değerinin basamak sayısı {sayac}");
+//int result = 0;
 //for (; ; )
 //{
 //    num /= 10;
@@ -208,15 +216,37 @@
 #region 3 basamaklı sayının basamaklarının küpleri toplamının sayının kendisine eşit olup olmadığı
 Console.WriteLine("3 basamaklı bir sayı giriniz");
 int num = Convert.ToInt32(Console.ReadLine());
-if (Basamak(num) == true)
+if (Basamak(num))
 {
+    int num_ = num;
+    double kupToplam = 0;
+    while (true)
+    {
+        int basamak = num_ % 10;        
+        num_ /= 10;
 
+        kupToplam += Math.Pow(kupToplam, 3);
+        
+        if (num_ < 10)
+        {
+            basamak = num_;
+            kupToplam += Math.Pow(basamak, 3);
+            break;
+        }
+            
+    }
+    if(kupToplam == num)
+        Console.WriteLine("Eşittir");
+    else
+        Console.WriteLine("Eşit değildir");
 }
+else
+    Console.WriteLine("Lütfen 3 basamaklı sayı giriniz");
 
 static bool Basamak(int num)
 {
     int basamakSayisi = 1;
-    while(true)
+    while (true)
     {
         basamakSayisi++;
         num /= 10;
